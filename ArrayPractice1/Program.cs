6b8814bac34190e7ce9d001bb76cf54502c4aa0d
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-06-30 11:17:31 +0530

    Add project files.

---
+++ ArrayPractice1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayPractice1
+{
+    // Program to take 5 values from the user and store them in an array
+    // Print the elements stored in the array
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 9, 6, 8, 10 };
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
